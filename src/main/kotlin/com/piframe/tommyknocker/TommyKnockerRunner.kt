bde4d2f3249c68fa31646048f1d5d7afd980450a
wasartin <watisartin@gmail.com>
2023-08-17 17:37:47 -0500

    Got to a working state. Can start up other gradle project with Runtime.exec

--- src/main/kotlin/com/piframe/tommyknocker/TommyKnockerRunner.kt
+++ src/main/kotlin/com/piframe/tommyknocker/TommyKnockerRunner.kt
@@ -1,11 +1,10 @@
 package com.piframe.tommyknocker
 
 import com.piframe.tommyknocker.model.Album
-import jakarta.annotation.PostConstruct
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.io.File
 import java.time.LocalTime
+
 
 @Service
 class TommyKnockerRunner() {
@@ -19,6 +18,7 @@ class TommyKnockerRunner() {
         //var directoryFilePath = "/home/piframe/dev/photo-db/posters/jpg/"
         var directoryFilePath = "/Users/wsartin/dev/workshop/photo-db/posters/jpg"
         var intervalInMinutes = 15
+        // TODO: change to ENV Vars
 //        if (args.isNotEmpty()) {
 //            directoryFilePath = args[0]
 //            intervalInMinutes = args[1].toInt()
@@ -53,7 +53,58 @@ class TommyKnockerRunner() {
      * Call Kotlin Display program
      */
     fun showPoster(image: File) {
-        //TODO
-        println("SHOWING POSTER ${image.absolutePath}")
+        val command = "../display/gradlew -p ../display run"
+        try {
+            Runtime.getRuntime().exec(command)
+        } catch (e: Exception){
+            println("Oh Shoot")
+            println(e)
+        }
+
+
+
+
     }
+
+    // Other way
+//    try{
+//        val process: Process = processBuilder.start()
+//        val reader = BufferedReader(InputStreamReader(process.inputStream))
+//        val line = reader.readText();
+//        val exitVal = process.waitFor()
+//        if (exitVal == 0) {
+//            println("Success!")
+//            exitProcess(0)
+//        } else {
+//            println("Something bad")
+//            println(line)
+//            exitProcess(0)
+//        }
+//    } catch(e: Exception) {
+//        println("Something went wrong")
+//        println(e)
+//    }
+
+
+//    val GRADLE_INSTALLATION = "/usr/local/Cellar/gradle/8.3"
+//
+//    val GRADLE_PROJECT_DIRECTORY = "~/dev/workshop/display/";
+//    val GRADLE_TASK = "run";
+//
+//    val gradleConn = GradleConnector.newConnector();
+//    gradleConn.useInstallation(File(GRADLE_INSTALLATION));
+//    gradleConn.forProjectDirectory(File(GRADLE_PROJECT_DIRECTORY));
+//
+//    val connection: ProjectConnection = gradleConn.connect()
+//    val build = connection.newBuild()
+//    build.forTasks(GRADLE_TASK)
+//
+//    println("About to start Gradle")
+//    try{
+//        build.run()
+//        connection.close()
+//    } catch(e: Exception) {
+//        println("Well shoot")
+//        println(e)
+//    }
 }
