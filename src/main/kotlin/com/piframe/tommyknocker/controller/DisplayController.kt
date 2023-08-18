12aeb80daadd8df4a694f2e37ff18d8ff482ae27
wasartin <watisartin@gmail.com>
2023-08-18 15:02:13 -0500

    setting up endpoint

--- src/main/kotlin/com/piframe/tommyknocker/controller/DisplayController.kt
+++ src/main/kotlin/com/piframe/tommyknocker/controller/DisplayController.kt
@@ -13,26 +13,52 @@ class DisplayController {
      */
     @GetMapping()
     fun getListOfAvailableImageFilesToDisplay(): ResponseEntity<String> {
-        return ResponseEntity<String>(HttpStatus.ACCEPTED)
+        println("Listing current files of available images to display")
+        return ResponseEntity<String>(HttpStatus.BAD_REQUEST)
     }
 
     /**
      * Receive a directory location of image files to cycle through
      */
+    @PostMapping()
+    fun setImageDirectory(): ResponseEntity<String>{
+        println("Setting files to iterate to given image Directory")
+        return ResponseEntity<String>(HttpStatus.BAD_REQUEST)
+    }
 
     /**
      * Request specific image to display
      */
+    @PutMapping()
+    fun setImageToDisplay(): ResponseEntity<String>{
+        println("Display image requested")
+        return ResponseEntity<String>(HttpStatus.BAD_REQUEST)
+    }
 
     /**
      * Request next image to display
      */
+    @PutMapping()
+    fun nextImage(): ResponseEntity<String>{
+        println("Going to the next image")
+        return ResponseEntity<String>(HttpStatus.BAD_REQUEST)
+    }
 
     /**
      * Request previous image to display
      */
+    @PutMapping()
+    fun previousImage(): ResponseEntity<String>{
+        println("Going back to the previous image")
+        return ResponseEntity<String>(HttpStatus.BAD_REQUEST)
+    }
 
     /**
      * Request set time for display interval
      */
+    @PutMapping()
+    fun setTimeForDisplayInterval(): ResponseEntity<String>{
+        println("Updating time for display interval")
+        return ResponseEntity<String>(HttpStatus.BAD_REQUEST)
+    }
 }
